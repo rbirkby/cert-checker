276963cc8f23798cdc45a703dae4fd5c972dee82
Richard Birkby <rbirkby@gmail.com>
2018-01-26 11:51:35 +0000

    Add SHA256 thumbprint and SAN values

--- cert-checker.sh
+++ cert-checker.sh
@@ -1,11 +1,17 @@
 #!/bin/bash -e
 
 for DOMAIN in `cat domains.txt`; do
-    IFS="=" CERTDATE=(`echo | openssl s_client -connect $DOMAIN:443 2>/dev/null | openssl x509  -noout -enddate`)
+    IFS="=" CERTDATE=(`echo | openssl s_client -servername $DOMAIN -connect $DOMAIN:443 2>/dev/null | openssl x509 -noout -enddate`)
     ENDDATE=${CERTDATE[1]}
 
-    IFS=$'\n' CHAIN=(`echo -n | openssl s_client -connect $DOMAIN:443 2>/dev/null | sed -ne '/Certificate chain/,/---/p'`)
+    IFS=$'\n' CHAIN=(`echo | openssl s_client -servername $DOMAIN -connect $DOMAIN:443 2>/dev/null | sed -ne '/Certificate chain/,/---/p'`)
     ROOT=${CHAIN[-2]}
 
-    printf "$DOMAIN; $ENDDATE; $ROOT\n"
+    IFS="=" FINGERPRINT=(`echo | openssl s_client -servername $DOMAIN -connect $DOMAIN:443 2>/dev/null | openssl x509 -noout -fingerprint -sha256`)
+    SHA256THUMBPRINT=${FINGERPRINT[1]}
+    THUMBPRINT=`echo $SHA256THUMBPRINT | sed -r 's/://g'`
+
+    SAN=`echo | openssl s_client -servername $DOMAIN -connect $DOMAIN:443 2>/dev/null | openssl x509 -noout -text | grep -A1 'Subject Alternative Name' | tail -n1`
+
+    printf "$DOMAIN; $ENDDATE; $ROOT; $THUMBPRINT; $SAN\n"
 done
